cdd7b59be3cbd3aee8ba4d78eec4870988348c82
Tim Perry <pimterry@gmail.com>
2026-09-08 18:45:03 +0200

    Reenable Flutter hooking in the Android Frida scripts

--- src/interceptors/frida/frida-scripts.ts
+++ src/interceptors/frida/frida-scripts.ts
@@ -41,7 +41,8 @@ export async function buildAndroidFridaScript(
             ['android', 'android-system-certificate-injection.js'],
             ['android', 'android-certificate-unpinning.js'],
             ['android', 'android-certificate-unpinning-fallback.js'],
-            ['android', 'android-disable-root-detection.js']
+            ['android', 'android-disable-root-detection.js'],
+            ['android', 'android-disable-flutter-certificate-pinning.js']
         ].map((hookRelPath) =>
             fs.readFile(path.join(FRIDA_SCRIPTS_ROOT, ...hookRelPath), { encoding: 'utf8' })
         )
